13cfe95abf2630559a07d71a4c835d6ae98b7ef1
hellonaeunkim <hellonaeunkim@gmail.com>
2026-02-13 17:29:02 +0900

    test: Stock 도메인 테스트 편의성을 위한 Fixture 클래스 추가

---
+++ product/src/test/java/com/hubEleven/product/stock/application/fixtures/StockFixture.java
@@ -0,0 +1,29 @@
+package com.hubEleven.product.stock.application.fixtures;
+
+import com.hubEleven.product.domain.model.Product;
+import com.hubEleven.stock.domain.model.Stock;
+import com.hubEleven.stock.presentation.dto.request.StockRequests;
+
+public class StockFixture {
+
+    // ===== Factory Methods =====
+
+    public static Stock createFromProductWithQuantity(Product product, int quantity) {
+        return Stock.create(
+                product.getProductId(),
+                product.getCompanyId(),
+                product.getHubId(),
+                quantity
+        );
+    }
+
+    public static StockRequests.Decrease decreaseRequest(Product product, int quantity) {
+        return new StockRequests.Decrease(
+                product.getProductId(),
+                quantity
+        );
+    }
+
+    private StockFixture() {
+    }
+}
