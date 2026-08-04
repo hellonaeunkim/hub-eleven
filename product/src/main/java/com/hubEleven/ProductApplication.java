b4ef400295999abab60b84dc1c9003c6f68a8630
hellonaeunkim <hellonaeunkim@gmail.com>
2026-08-04 21:13:15 +0900

    fix: Product 서비스에 공통 예외 처리기 등록

--- product/src/main/java/com/hubEleven/ProductApplication.java
+++ product/src/main/java/com/hubEleven/ProductApplication.java
@@ -1,13 +1,16 @@
 package com.hubEleven;
 
+import com.commonLib.common.exception.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@Import(GlobalExceptionHandler.class)
 public class ProductApplication {
 
 	public static void main(String[] args) {
